86f2bc63500bc6f40177090a849bd77c6275f1aa
far1h <farihmuhammad45@gmail.com>
2024-11-25 17:34:27 +0700

    Bringing In Boxes

--- AngryBirdClone/GameScene.swift
+++ AngryBirdClone/GameScene.swift
@@ -11,25 +11,82 @@ import GameplayKit
 class GameScene: SKScene {
     
     var bird = SKSpriteNode()
+    var box1 = SKSpriteNode()
+    var box2 = SKSpriteNode()
+    var box3 = SKSpriteNode()
+    var box4 = SKSpriteNode()
+    var box5 = SKSpriteNode()
+    var box6 = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
-//        let texture = SKTexture(imageNamed: "bird")
-//        bird2 = SKSpriteNode(texture: texture)
-//        bird2.position = CGPoint(x: -self.frame.width / 3, y: -self.frame.height / 6 )
-//        bird2.size = CGSize(width: self.frame.width / 16, height: self.frame.width / 16)
-//        bird2.zPosition = 1
-//        self.addChild(bird2)
+        //        let texture = SKTexture(imageNamed: "bird")
+        //        bird2 = SKSpriteNode(texture: texture)
+        //        bird2.position = CGPoint(x: -self.frame.width / 3, y: -self.frame.height / 6 )
+        //        bird2.size = CGSize(width: self.frame.width / 16, height: self.frame.width / 16)
+        //        bird2.zPosition = 1
+        //        self.addChild(bird2)
         
+        // Physics Body
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        self.scene?.scaleMode = .aspectFit
+        
+        // Bird
         bird = childNode(withName: "bird") as! SKSpriteNode
         
         let birdTexture = SKTexture(imageNamed: "bird")
         bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height / 16)
-        bird.physicsBody?.affectedByGravity = false
+        bird.physicsBody?.affectedByGravity = true
         bird.physicsBody?.isDynamic = true
         bird.physicsBody?.mass = 0.5
         
-        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        // Box
+        
+        
+        let boxTexture = SKTexture(imageNamed: "brick")
+        let size =  CGSize(width: boxTexture.size().width / 6.5, height: boxTexture.size().height / 6.5)
+        box1 = childNode(withName: "box1") as! SKSpriteNode
+        box1.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box1.physicsBody?.affectedByGravity = true
+        box1.physicsBody?.isDynamic = true
+        box1.physicsBody?.allowsRotation = true
+        box1.physicsBody?.mass = 0.5
+        
+        box2 = childNode(withName: "box2") as! SKSpriteNode
+        box2.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box2.physicsBody?.affectedByGravity = true
+        box2.physicsBody?.isDynamic = true
+        box2.physicsBody?.allowsRotation = true
+        box2.physicsBody?.mass = 0.5
+        
+        box3 = childNode(withName: "box3") as! SKSpriteNode
+        box3.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box3.physicsBody?.affectedByGravity = true
+        box3.physicsBody?.isDynamic = true
+        box3.physicsBody?.allowsRotation = true
+        box3.physicsBody?.mass = 0.5
+        
+        box4 = childNode(withName: "box4") as! SKSpriteNode
+        box4.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box4.physicsBody?.affectedByGravity = true
+        box4.physicsBody?.isDynamic = true
+        box4.physicsBody?.allowsRotation = true
+        box4.physicsBody?.mass = 0.5
+        
+        box5 = childNode(withName: "box5") as! SKSpriteNode
+        box5.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box5.physicsBody?.affectedByGravity = true
+        box5.physicsBody?.isDynamic = true
+        box5.physicsBody?.allowsRotation = true
+        box5.physicsBody?.mass = 0.5
+        
+        box6 = childNode(withName: "box6") as! SKSpriteNode
+        box6.physicsBody = SKPhysicsBody(rectangleOf: size)
+        box6.physicsBody?.affectedByGravity = true
+        box6.physicsBody?.isDynamic = true
+        box6.physicsBody?.allowsRotation = true
+        box6.physicsBody?.mass = 0.5
+        
         
     }
     
@@ -47,19 +104,19 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-       
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
     }
     
     
